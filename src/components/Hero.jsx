c0b4f1e5b424a99d0a835922fe8dde78ac1a9967
streetdevelopment <streetdevbusiness@gmail.com>
2024-10-16 23:16:02 +0800

    Layout Initialization

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <h1 class="main-heading">Welcome to my To Do List!</h1>
+            <div className="hero-cta">
+                <button className="login-btn"><h2>Login</h2></button>
+                <button className="get-started-btn"><h2>Get Started</h2></button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
